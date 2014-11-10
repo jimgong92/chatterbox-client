bc6d4d6bd03ce09e23c561789fa4cd5c98a4450c
jimgong92 <gong.jim@gmail.com>
2014-11-10 15:22:55 -0800

    Implement ajax message retrieval

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,18 @@
 // YOUR CODE HERE:
+var serverURL = "https://api.parse.com/1/classes/chatterbox";
 
+$.ajax({
+  url: serverURL,
+  type: "GET",
+  success: function(json) {
+    var arr = json.results;
+    _.each(arr, function(item) {
+      var $res = $('<li></li>');
+      $res.text(item.username + ": " + item.text);
+      $('#messages').append($res);
+    });
+  },
+  error: function() {
+    console.log("!!!");
+  }
+});
