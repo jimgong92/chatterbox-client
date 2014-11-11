05bd5493e7314942552fa372b61e533cb62e22c2
jimgong92 <gong.jim@gmail.com>
2014-11-10 16:29:40 -0800

    Allows user input

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,18 +1,51 @@
 // YOUR CODE HERE:
-var serverURL = "https://api.parse.com/1/classes/chatterbox";
-
-$.ajax({
-  url: serverURL,
-  type: "GET",
-  success: function(json) {
-    var arr = json.results;
-    _.each(arr, function(item) {
-      var $res = $('<li></li>');
-      $res.text(item.username + ": " + item.text);
-      $('#messages').append($res);
+$(document).ready(function() {
+  var retrieve = function() {
+    $.ajax({
+      type: "GET",
+      data:'order=-createdAt',
+      success: function(json) {
+        var arr = json.results;
+        $('li').remove();
+        _.each(arr, function(item) {
+          var $res = $('<li></li>');
+          $res.text(item.username + ": " + item.text);
+          $('#messages').append($res);
+        });
+      },
+      error: function() {
+        console.log("!!!");
+      }
     });
-  },
-  error: function() {
-    console.log("!!!");
-  }
+  };
+
+  var send = function(data) {
+    $.ajax({
+      type: "POST",
+      data: data,
+      contentType: 'application/json'
+    });
+  };
+
+  setInterval(retrieve, 1000);
+
+  var userName = window.location.search.substring(window.location.search.indexOf("=") + 1);
+
+  $('#submitButton').on('click', function(){
+    //calculate data to transmit
+    console.log($('#input').val() );
+    var data = JSON.stringify({
+      'username': userName,
+      'text' : $('#input').val()
+    });
+    console.log("!!");
+    send(data);
+    $('#input').val("");
+
+    // var $newPost = $('<li></li>');
+    // var $text = $('#input').text();
+    // $newPost.text(userName + ": " + $text);
+    // $('#messages').prepend($newPost);
+  });
+
 });
